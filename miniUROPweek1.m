4531a6f0c82b6621c41af1f17ec0c35446318249
cjdooley123 <51967067+cjdooley123@users.noreply.github.com>
2023-01-16 13:21:08 -0500

    Fixed bug with rotation and added new example code
    X was changing before being applied to rotate Y, so defined new X_rot and Y_rot variables to avoid changing mid calculation

--- miniUROPweek1.m
+++ miniUROPweek1.m
@@ -8,31 +8,25 @@ double_frame = double(gray_frame);
 height = size(video_frame, 1);
 width = size(video_frame, 2);
 
-y_pixel = 1:(width);
-x_pixel = 1:(height);
-[Y,X] = meshgrid(y_pixel,x_pixel);
+y_pixel = -(1:(height));
+x_pixel = 1:(width);
+[X,Y] = meshgrid(x_pixel,y_pixel);
 
-
-ang2 = 171.5;
-Y = Y*cosd(ang2)-X*sind(ang2);
-X = Y*sind(ang2)+X*cosd(ang2);
+ang2 = 278.5;
+X_rot = X*cosd(ang2)-Y*sind(ang2);
+Y_rot = X*sind(ang2)+Y*cosd(ang2);
 
 offset_x = 2832.21-87.3912 +(95.19/0.018); %+ (104.446/0.002647)
 offset_y = 4133.8-16.7108+(575.803/0.018);%610.861/0.002647
-X = X + offset_x;
-Y = Y+ offset_y;
+X = X_rot + offset_x;
+Y = Y_rot + offset_y;
 
 scale_factor = 0.018; %0.002647
 X = X*scale_factor;
 Y = Y*scale_factor;
 
-X_old = X;
-Y_old = Y;
-
-X = Y_old;
-Y = X_old;
-
-pcolor_frame = pcolor(Y, X, double_frame);
+figure
+pcolor_frame = pcolor(X, Y, double_frame);
 set(pcolor_frame, 'EdgeColor', 'none');
 daspect([1 1 1]);
 
@@ -40,16 +34,17 @@ min_x = min(X(:));
 min_y = min(Y(:));
 max_x = max(X(:));
 max_y = max(Y(:));
-resolution = 0.018;
+resolution = 0.2;
 
 x_frf = min_x:resolution:max_x;
-y_frf = min_y:resolution:min_y;
+y_frf = min_y:resolution:max_y;
 [X_grid, Y_grid] = meshgrid(x_frf,y_frf);
 
-F = scatteredInterpolant(Y,X,double_frame);
+F = scatteredInterpolant(X(:),Y(:),double_frame(:),'linear','none');
 video_frame_grid = F(X_grid,Y_grid);
 
-pcolor(X_grid,Y_grid,video_frame_grid)
+figure
+pcolor_frame = pcolor(X_grid,Y_grid,video_frame_grid);
 set(pcolor_frame, 'EdgeColor', 'none');
 daspect([1 1 1]);
 
